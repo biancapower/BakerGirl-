d40f3211cbdbc12b417a93a778f325a4de3ba416
Tessa <Tessa>
2018-01-11 14:46:56 +1100

    about content

--- react-web/src/pages/AboutPage.js
+++ react-web/src/pages/AboutPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Jumbotron, Container } from 'reactstrap';
+import { Jumbotron, Container, Row, Col, Card, CardTitle, CardText } from 'reactstrap';
 import { Media } from 'reactstrap';
 import BackgroundPicture from '../assets/images/BackgroundPicture.png';
 import Logo from '../assets/images/Logo.png';
@@ -8,16 +8,16 @@ import Logo from '../assets/images/Logo.png';
 export default function () {
   return (
     <div>
-    <div className="Jumbotron">
-       <Container fluid>
-            <Media width = "100%" height ="10%" object src={BackgroundPicture} alt="Generic placeholder image" />
-       </Container>
-    </div>
-
+    <Jumbotron fluid></Jumbotron>
+      <Container>
      <div className="logo">
           <Media id="logo" object src={Logo} alt="Generic placeholder image" />
      </div>
-
-   </div>
+        <Card id="about-card">
+          <CardTitle>ABOUT</CardTitle>
+              <CardText>Creating delicious cakes, macarons and miniature desserts for your special event or just because. Delivering to you all over Sydney.</CardText>
+        </Card>
+      </Container>
+    </div>
   )
 }

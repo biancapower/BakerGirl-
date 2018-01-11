f7d0365b009655642ba6307e0dcce80d69ca547e
Tessa <Tessa>
2018-01-11 13:22:51 +1100

    added logo and background image

--- react-web/src/pages/AboutPage.js
+++ react-web/src/pages/AboutPage.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Jumbotron, Container } from 'reactstrap';
+import { Media } from 'reactstrap';
+import BackgroundPicture from '../assets/images/BackgroundPicture.png';
+import Logo from '../assets/images/Logo.png';
+
 
 export default function () {
   return (
-    <span>WELCOME TO ABOUT</span>
+    <div>
+    <div className="Jumbotron">
+       <Container fluid>
+            <Media width = "100%" height ="10%" object src={BackgroundPicture} alt="Generic placeholder image" />
+       </Container>
+    </div>
+
+     <div className="logo">
+          <Media id="logo" object src={Logo} alt="Generic placeholder image" />
+     </div>
+
+   </div>
   )
 }

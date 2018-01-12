41039635ba1f8cebb70a0391411d6a7f75a4f41d
Bianca Power <drbiancampower@gmail.com>
2018-01-12 14:56:06 +1100

    styling

--- react-web/src/components/Cake.js
+++ react-web/src/components/Cake.js
@@ -4,50 +4,64 @@ import Comment from './Comment'
 export default function Cake(props) {
   const { layers, tiers, flavours, fillings, style, custName, contactNumber, email, eventDate, eventType, servings, delivery, deliveryAddress, referral } = props;
   return (
-    <div className="cake mt-5">
-      <div>
-        Layers: {layers}
-      </div>
-      <div>
-        Tiers: {tiers}
-      </div>
-      <div>
-        Flavours: {flavours}
-      </div>
-      <div>
-        Fillings: {fillings}
-      </div>
-      <div>
-        Style: {style}        
-      </div>
-      <div>
-        Customer Name: {custName}
-      </div>
-      <div>
-        Contact Number: {contactNumber}
-      </div>
-      <div>
-        Email Address: {email}
-      </div>
-      <div>
-        Event Date: {eventDate}
-      </div>
-      <div>
-        Event Type: {eventType}
-      </div>
-      <div>
-        Number of Servings: {servings}
-      </div>
-      <div>
-        Delivery Required?: {delivery ? "yes" : "no"}
-      </div>
-      <div>
-        Delivery Address: {deliveryAddress}
-      </div>
-      <div>
-        Where heard about BG: {referral}
-      </div>
+    <table className="cake admin-table mt-5 mb-5">
+      <tr>
+        <th className="admin-pg-q">Customer Name:</th> 
+        <th className="admin-pg-a">{custName}</th>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Contact Number:</td> 
+        <td className="admin-pg-a">{contactNumber}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Email Address:</td> 
+        <td className="admin-pg-a">{email}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Event Date:</td> 
+        <td className="admin-pg-a">{eventDate}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Event Type:</td> 
+        <td className="admin-pg-a">{eventType}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Layers:</td> 
+        <td className="admin-pg-a">{layers}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Tiers:</td> 
+        <td className="admin-pg-a">{tiers}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Flavours:</td> 
+        <td className="admin-pg-a">{flavours}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Fillings:</td> 
+        <td className="admin-pg-a">{fillings}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Style:</td> 
+        <td className="admin-pg-a">{style}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Number of Servings:</td> 
+        <td className="admin-pg-a">{servings}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Delivery Required?:</td> 
+        <td className="admin-pg-a">{delivery ? "yes" : "no"}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Delivery Address:</td> 
+        <td className="admin-pg-a">{deliveryAddress}</td>
+      </tr>
+      <tr>
+        <td className="admin-pg-q">Where heard about BG:</td> 
+        <td className="admin-pg-a">{referral}</td>
+      </tr>
 
-    </div>
+    </table>
   )
 }

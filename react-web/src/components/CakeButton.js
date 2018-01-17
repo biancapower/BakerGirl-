4918910a039b9e482125ff58bdb5ea2192b85434
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2018-01-17 16:08:32 +1100

    update background image and font colour

--- react-web/src/components/CakeButton.js
+++ react-web/src/components/CakeButton.js
@@ -10,14 +10,11 @@ export default function CakeButton() {
     <div>
       <Jumbotron className="home-jumbo">
 
-          <h1 className="display-5">Hello, world!</h1>
-          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr className="my-2" />
-          <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
-          <p className="lead">
-          <Link to="/CreateACake"><Button class="primary">Create A Cake</Button></Link>
+          <h1 className="display-6">BAKERGIRL</h1>
+          <p className="lead">Hi I'm Erin and welcome to BakerGirl, a home for all things sweet</p>
 
-          </p>
+          <Link to="/CreateACake"><Button className="button">Create A Cake</Button></Link>
+
         </Jumbotron>
     </div>
   </Route>

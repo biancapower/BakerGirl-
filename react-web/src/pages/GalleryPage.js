fb363f802715e8b1fcaf93b63a2d46121dc07e87
Bianca Power <drbiancampower@gmail.com>
2018-01-22 14:08:35 +1100

    add call to action on gallery page

--- react-web/src/pages/GalleryPage.js
+++ react-web/src/pages/GalleryPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import InstaFeed from '../components/InstaFeed';
-import { Jumbotron, Container, Row, Col, Card, CardTitle, CardText } from 'reactstrap';
+import { Jumbotron, Container, Row, Col, Card, CardTitle, CardText, Media } from 'reactstrap';
+import Instagram from '../assets/images/instagram-logo.png';
 
 
 export default function () {
   return (
     <Container className="cont">
       <Col>
+        <Row className="m-5 pt-3 text-center">
+          <Col>
+            <h2><a href="https://www.instagram.com/_bakergirl/" target="_blank"><Media id="insta-logo" object src={Instagram} alt="Instagram Logo" />Follow us on Instagram</a></h2>
+          </Col>
+        </Row>
         <Row className="m-5 text-center">
           <InstaFeed />
         </Row>   

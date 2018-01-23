fcb2d462b49025a1d5324e10c294acd9f40906f9
Bianca Power <drbiancampower@gmail.com>
2018-01-23 21:11:26 +1100

    try a * for CORS

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ app.use(authMiddleware.initialize);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 

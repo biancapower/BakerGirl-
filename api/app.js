654c407bee49826871a3d419ad58e621c960daf9
Bianca Power <drbiancampower@gmail.com>
2018-01-23 14:18:28 +1100

    test

--- api/app.js
+++ api/app.js
@@ -21,7 +21,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.options("*", cors());
+app.use(cors());
+// app.options("*", cors());
 
 // cakes router/controller
 const cakesRouter = require('./routes/cakes');

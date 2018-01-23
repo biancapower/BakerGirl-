14d51f8f44f721ae316d02cc16fac57253812142
Bianca Power <drbiancampower@gmail.com>
2018-01-23 11:56:19 +1100

    try env change

--- api/models/base.js
+++ api/models/base.js
@@ -3,6 +3,6 @@ mongoose.Promise = Promise;
 const db = mongoose.connection;
 console.log('mongourl', process.env.MONGO_URI)
 // db.on('open', () => { console.log('Successful connection to MongoDB')});
-mongoose.connect('mongodb://admin:cakes@ds249757.mlab.com:49757/cakes-api', { useMongoClient: true });
+mongoose.connect(process.env.MONGO_URI, { useMongoClient: true });
 
 module.exports = mongoose;

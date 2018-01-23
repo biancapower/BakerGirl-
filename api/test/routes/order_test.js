9801053e2f968c80ee3dea7bd0a2b89e201372f9
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2018-01-23 16:46:08 +1100

    fixed tests

--- api/test/routes/order_test.js
+++ api/test/routes/order_test.js
@@ -1,17 +1,19 @@
-
 const chai = require('chai');
 const expect = chai.expect;
 const app = require('../../app');
 const Cake = require('../../models/cake');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
 
 it("should return a list of cake order", function() {
-  chai.request(app)
+   chai.request(app)
   .get('/cakes')
 });
 
 
 it("create a new cake", function() {
-   chai.request(app)
+    chai.request(app)
   .post('/CreateACake')
   .then(function (err, res) {
     expect(err).to.be.json;
@@ -20,7 +22,7 @@ it("create a new cake", function() {
 });
 
 it("request for authentication", function() {
-   chai.request(app)
+    chai.request(app)
    .post('/signin')
    .auth('email', 'password')
 });
